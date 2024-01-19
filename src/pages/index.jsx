92042b987720cec7a268cd514c41ba83e883a37a
Phelipe <phelipesenac@gmail.com>
2024-01-19 10:33:48 -0300

    formataço do article

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,11 +15,48 @@ export default function Home() {
       </Head>
       <StyledHome>
         <h2>Pet Notícias</h2>
+        <StyledListaPosts>
+          <article>
+            <Link href="">
+              <h3>Título do post...</h3>
+              <p>Subtítulo do post</p>
+            </Link>
+          </article>
+          <article>
+            <Link href="">
+              <h3>Título do post...</h3>
+              <p>Subtítulo do post</p>
+            </Link>
+          </article>
+        </StyledListaPosts>
       </StyledHome>
     </>
   );
 }
+const StyledListaPosts = styled.div`
+  article {
+    background-color: #f7f7f7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: var(--sombra-box);
+    border-radius: var(--borda-arredondada);
+    transition: transform 200ms;
+  }
+  article:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
 
+  & a {
+    text-decoration: none;
+    color: black;
+  }
+
+  &:hover,
+  &:focus {
+    color: #0066ff;
+  }
+`;
 const StyledHome = styled.section`
   h2::before {
     content: "📰 ";
