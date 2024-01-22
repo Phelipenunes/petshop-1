3283b840cfa2e2403dfe21b6942f8db2b7a607d3
Phelipe <phelipesenac@gmail.com>
2024-01-22 10:41:13 -0300

    .
    .

--- src/components/ListaPosts.jsx
+++ src/components/ListaPosts.jsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export default function ListaPosts({ posts }) {
+  if (posts.length === 0)
+    return <h3 style={{ textAlign: "center" }}>Ainda não há posts</h3>;
+
   return (
     <StyledListaPosts>
       {posts.map((post) => {
