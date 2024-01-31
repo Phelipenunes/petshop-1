756953d0daf1f2eb00aecef72d0ac21a87820f76
Phelipe <phelipesenac@gmail.com>
2024-01-31 09:42:37 -0300

    Update container.jsx

--- src/components/ui/container.jsx
+++ src/components/ui/container.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export default function Container({ children }) {
   return <StyledContainer>{children}</StyledContainer>;
 }
+
 const StyledContainer = styled.div`
   width: 100%;
   background-color: #f7f7f7;
